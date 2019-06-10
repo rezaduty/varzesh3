54dc8ac6355bd6ce0a7d8e73fbf3a9fb52d53325
rezaduty <reza.duty.persian@gmail.com>
2019-06-10 21:55:33 +0430

    add simple server

--- src/components/Left_Ads.js
+++ src/components/Left_Ads.js
@@ -1,47 +1,42 @@
 import React from "react";
 
+
+const API = 'https://sample.api.com/api/';
+const DEFAULT_QUERY = 'redux';
+
 class Left_Ads extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hits: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(API + DEFAULT_QUERY)
+      .then(response => response.json())
+      .then(data => this.setState({ hits: data.hits }));
+  }
+
+
+
   render() {
+    const { hits } = this.state;
     return (
-        <div className="job-ad">
-            <a href="/Telegram" title="ورزش سه را در تلگرام دنبال کنید" target="_blank" id="desktop-telegram">
-                <img src="http://static2.farakav.com/varzesh3/assets/img/ads/telegram-banner-90.gif" alt="کانال تلگرام ورزش سه" />
-            </a>
-      
+         <div className="ad-container row-fluid" data-adposition="6">
+        {hits.map(hit =>
+            <div data-extension="jpg" className="ad undefined">
 
-    <div className="ad-container row-fluid" data-adposition="6">
-        <div data-extension="jpg" className="ad undefined">
-
-            <a href="http://ads.farakav.com/clk?av=BuYN&amp;gl=b743736762f8477cb2fab798dde58a42&amp;lc=0" target="_blank">
-                <img width="130"  height="400" alt="" src="https://static.farakav.com/v3/static/bpx/00910854.jpg" />
+            <a href="{hit.link}" target="_blank">
+                <img width="130"  height="400" alt="" src="{hit.image}" />
             </a>
-        </div>
-        <div data-extension="gif" className="ad undefined">
+           </div>
+        )}
+      </div>
 
-            <a href="http://ads.farakav.com/clk?av=GOYN&amp;gl=b8b51095eb327447f32cb4cce85084c9&amp;lc=1" target="_blank">
-                <img width="130"  height="400" alt="" src="https://static.farakav.com/v3/static/bpx/00910872.gif" />
-            </a>
-        </div>
-        <div data-extension="gif" className="ad undefined">
-
-            <a href="http://ads.farakav.com/clk?av=EeYN&amp;gl=3684046e43eb75d909f573ddddb3c8fe&amp;lc=2" target="_blank">
-                <img width="130"  height="200" alt="" src="https://static.farakav.com/v3/static/bpx/00910865.gif" />
-            </a>
-        </div>
-        <div data-extension="gif" className="ad undefined">
-
-            <a href="http://ads.farakav.com/clk?av=DuYN&amp;gl=62ea62faa621897153b4ee05284e2cf3&amp;lc=3" target="_blank">
-                <img width="130"  height="400" alt="" src="https://static.farakav.com/v3/static/bpx/00910862.gif" />
-            </a>
-        </div>
-        <div data-extension="gif" className="ad undefined">
-
-            <a href="http://ads.farakav.com/clk?av=8eUN&amp;gl=01499dfe69be2f48fbdaae6b040f8e37&amp;lc=4" target="_blank">
-                <img width="130"  height="200" alt="" src="https://static.farakav.com/v3/static/bpx/00910833.gif" />
-            </a>
-        </div>
-    </div>
-          </div>
+        
        
     );
   }
